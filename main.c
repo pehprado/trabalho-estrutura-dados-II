18878d6bd2809c31c4d0057461f12db95a5d06e0
PedroHenrique13 <pedrohenrique.pradooliveira13@gmail.com>
2017-10-07 13:09:27 -0300

    [Fix] Ajustes nas funções make_tree e get_index.

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "abb.h"
 
-void make_tree(arvbin * t, int n, int in_i, int in[], int pos_i, int pos[]);
+void make_tree(arvbin *t, int in_f, int in_i, int in[], int pos_i, int pos_f, int pos[]);
 int get_index(int arr[], int comeco, int fim, int valor);
 
 int main()
@@ -23,7 +23,7 @@ int main()
     //for (i = 0; i < n; i++)
       //  scanf("%d", &posOrdem[i]);
 
-     make_tree(&t, n-1, 0, emOrdem, 0, posOrdem);
+     make_tree(&t, 0, n-1, emOrdem, 0, n-1, posOrdem);
 
     //int indice;
 
@@ -34,30 +34,41 @@ int main()
     return 0;
 }
 
-void make_tree(arvbin * t, int n, int in_i, int in[], int pos_i, int pos[]) {
+void make_tree(arvbin *t, int in_i, int in_f, int in[], int pos_i, int pos_f, int pos[]) {
+    
+    if ((in_i > in_f) || (pos_i > pos_f)) return;
 
-    /*if (pos_i == 0) {
-        (*t)->dado = pos[pos_i];
-        (*t)->esq = (*t)->dir = NULL;
-    }
-    else {
-        (*t)->dado = pos[pos_i];
-        int indice = get_index(in, in_i, pos_i, pos[pos_i]);
-        make_tree(&t->esq, indice, 0, in, 0, pos);
-        make_tree(&t->dir, )
-    }*/
-    int dado = pos[n];
-    tree_ins(&(*t), pos[n]);
-    int indexIn = get_index(in, in_i, n, pos[n]);
+    int dado = pos[pos_f];
+    tree_ins(&(*t), dado);
 
-    if (indexIn > 0)
-        make_tree(&(*t)->esq, indexIn-1, 0, in, 0, pos);
-    if (indexIn < n)
-        make_tree(&(*t)->dir, n-1, indexIn, in, indexIn, pos);
+    //tree_ins(&(*t), pos[n]);
+    int indexIn = get_index(in, in_i, in_f, pos[pos_f]);
+
+    // Pegando o tamanho das próximas subárvores
+    int tamEsq = indexIn - in_i;
+    int tamDir = in_f - indexIn;
+
+    // Ajustando os valores das posições inicial e final da em-Ordem e pós-Ordem
+    int ini_emOrder = indexIn+1;
+    int fim_emOrder = in_f;
+    int ini_posOrder = pos_f-tamDir;
+    int fim_posOrder = pos_f-1;
+
+    // Chamada recursiva pra subárvores direita com os novos valores
+    make_tree(&(*t)->dir, ini_emOrder, fim_emOrder, in, ini_posOrder, fim_posOrder, pos);
+
+    // Ajustando os valores das posições inicial e final da em-Ordem e pós-Ordem
+    ini_emOrder = in_i;
+    fim_emOrder = indexIn - 1;
+    ini_posOrder = pos_f-tamDir-tamEsq;
+    fim_posOrder = pos_f-tamDir-1;
+
+    // Chamada recursiva pra subárvores esquerda com os novos valores
+    make_tree(&(*t)->esq, ini_emOrder, fim_emOrder, in, ini_posOrder, fim_posOrder, pos);
 }
 
 int get_index(int arr[], int comeco, int fim, int valor) {
-    int i = 0;
+    int i = comeco;
 
     while ((i<=fim) && (arr[i] != valor)) i++;
     return i;
